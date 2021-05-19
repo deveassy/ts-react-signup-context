fda25eebadc3d546cc529767b4d546d96d1e9555
deveassy <xjfmwld@gmail.com>
2021-05-19 17:39:25 +0900

    App파일에 submit 기능 추가, 화면출력 성공

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+type InputValue = {
+  name: string;
+  age: string;
+  email: string;
+};
+
 export default function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [totalValue, setTotalValue] = useState<InputValue>({
+    name: "",
+    age: "",
+    email: "",
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (!name || !age || !email) return alert("빈칸을 채우세요");
+    setTotalValue({
+      name,
+      age,
+      email,
+    });
+    setName("");
+    setAge("");
+    setEmail("");
+  };
+
   return (
     <Container>
       <ProfileContainer>
-        <ProfileText>이름 : </ProfileText>
-        <ProfileText>나이 : </ProfileText>
-        <ProfileText>이메일 : </ProfileText>
+        <ProfileText>이름 : {totalValue.name}</ProfileText>
+        <ProfileText>나이 : {totalValue.age}</ProfileText>
+        <ProfileText>이메일 : {totalValue.email}</ProfileText>
+        <ProfileText>{/*저장된 정보 없을 경우 보여줄 메세지*/}</ProfileText>
       </ProfileContainer>
       <FormContainer>
-        <SignupInput placeholder="이름" />
-        <SignupInput placeholder="나이" />
-        <SignupInput placeholder="이메일" />
-        <SignupInput placeholder="비밀번호" />
-        <SubmitButton>제출</SubmitButton>
+        <SignupInput
+          placeholder="이름"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <SignupInput
+          placeholder="나이"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <SignupInput
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <SubmitButton onClick={handleSubmit}>제출</SubmitButton>
       </FormContainer>
     </Container>
   );
