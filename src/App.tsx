741fe3dc8ee477b4d2f345035b95926288880b02
deveassy <xjfmwld@gmail.com>
2021-05-19 16:57:49 +0900

    App파일에 전체 레이아웃 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
-/import React, { useState } from "react";
-import Profile from "./components/Profile";
-import SignupForm from "./components/SignupForm";
+import React from "react";
+import styled from "styled-components";
 
-// import { ContextProvider } from "./contexts/Context";
-
-type AppProps = {
-  onSubmit: () => void;
-  newInfo: {
-    name: string;
-    age: number;
-    email: string;
-    pwd: string;
-  };
-};
-
-function App({ onSubmit }: AppProps) {
-  const [state, setState] = useState({
-   
-  });
+export default function App() {
   return (
-    // <ContextProvider>
-    <div>
-      <Profile>{state}</Profile>
-      <SignupForm onSubmit={onSubmit} />
-    </div>
-    // </ContextProvider>
+    <Container>
+      <ProfileContainer>
+        <ProfileText>이름 : </ProfileText>
+        <ProfileText>나이 : </ProfileText>
+        <ProfileText>이메일 : </ProfileText>
+      </ProfileContainer>
+      <FormContainer>
+        <SignupInput placeholder="이름" />
+        <SignupInput placeholder="나이" />
+        <SignupInput placeholder="이메일" />
+        <SignupInput placeholder="비밀번호" />
+        <SubmitButton>제출</SubmitButton>
+      </FormContainer>
+    </Container>
   );
 }
 
-export default App;
+const FlexDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled(FlexDiv)`
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const ProfileContainer = styled(FlexDiv)`
+  flex-direction: column;
+`;
+const ProfileText = styled.p``;
+const FormContainer = styled(FlexDiv)`
+  flex-direction: column;
+`;
+const SignupInput = styled.input``;
+const SubmitButton = styled.button``;
