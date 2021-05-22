800a082ef96fd78568ad80ea01b2efed2bca4510
deveassy <xjfmwld@gmail.com>
2021-05-22 15:25:58 +0900

    코드 리팩토링

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 
 type StateType = {
@@ -14,21 +14,17 @@ type ProfileProps = {
 // 받은 데이터 (props = value)를 제 위치에 뿌려주기만 하면 되는 컴포넌트
 export default function Profile(props: ProfileProps) {
   const { value } = props;
-  const [state, setState] = useState("회원정보가 없어요.");
-
-  useEffect(() => {
-    console.log("rendering");
-    return () => {
-      setState("");
-    };
-  }, [value]);
 
   return (
     <ProfileContainer>
       <ProfileText>이름 : {value.name}</ProfileText>
       <ProfileText>나이 : {value.age}</ProfileText>
       <ProfileText>이메일 : {value.email}</ProfileText>
-      <ProfileText>{state}</ProfileText>
+      <ProfileText>
+        {value.name && value.age && value.email
+          ? "안녕하세요 :)"
+          : "회원 정보가 없어요 :("}
+      </ProfileText>
     </ProfileContainer>
   );
 }
